99a9fc7645fa54be1a28b685412facae0aadf2c5
Lenarin <Vladislavakia425@gmail.com>
2022-12-05 18:42:30 +0300

    init

---
+++ layouts/default.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    <v-app-bar
+      clipped-left
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="isDrawerActive = !isDrawerActive" />
+      <v-toolbar-title>
+        <NuxtLink 
+          class="text-decoration-none white--text" 
+          to="/"
+        >ЭУМК React</NuxtLink>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <NavigationDrawer :value="isDrawerActive"/>
+  </v-app>
+</template>
+
+<script>
+import NavigationDrawer from '~/components/NavigationDrawer.vue';
+
+export default {
+  name: "DefaultLayout",
+  components: { NavigationDrawer },
+  data() {
+    return {
+      isDrawerActive: false
+    };
+  }
+}
+</script>
